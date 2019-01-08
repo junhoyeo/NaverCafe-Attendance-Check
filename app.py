a21c4514c1473966a5993cec6c258f64093a1861
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-01-08 14:36:06 +0900

    UPDATE: scheduler

--- app.py
+++ app.py
@@ -1,30 +1,40 @@
-import json, time, random
-import logging
+import time
 import os.path
+import schedule
 from selenium import webdriver
 
 URL = '???'
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-with open(os.path.join(CURRENT_DIR, './secret.json'), 'r') as file:
-    user = json.load(file)
 
-driver_path = os.path.join(CURRENT_DIR, './chromedriver')
-driver = webdriver.Chrome(driver_path)
+def check(driver):
+    try:
+        start_time = time.time()
+        driver.get(URL)
+        driver.find_element_by_css_selector('a#menuLink46').click()
+        driver.implicitly_wait(2)  # 대기
+        driver.switch_to.frame(driver.find_element_by_id('cafe_main'))
+        driver.find_element_by_css_selector(
+            'textarea#cmtinput').send_keys('><')
+        driver.find_element_by_css_selector(
+            'button#btn-submit-attendance').click()
+        driver.quit()
+        print((time.time() - start_time), 'success')
+    except BaseException:
+        check(driver)
 
-# login
-driver.get('https://nid.naver.com/nidlogin.login')
-input('press enter after login') # 로그인 대기
 
-driver.get(URL)
-driver.find_element_by_css_selector('a#menuLink46').click()
-# driver.implicitly_wait(5)
+def main():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    driver_path = os.path.join(current_dir, './chromedriver')
+    driver = webdriver.Chrome(driver_path)
 
-# driver.execute_script('''
-#     var cafe = document.getElementById('cafe_main').contentWindow.document;
-#     cafe.getElementById('cmtinput').value = '><';
-#     cafe.querySelector('button#btn-submit-attendance').click();
-# ''')
-driver.switch_to.frame(driver.find_element_by_id('cafe_main'))
-driver.find_element_by_css_selector('textarea#cmtinput').send_keys('><')
-driver.find_element_by_css_selector('button#btn-submit-attendance').click()
-driver.quit()
+    # login
+    driver.get('https://nid.naver.com/nidlogin.login')
+    input('press enter after login')  # 로그인 대기
+
+    schedule.every().day.at('00:00').do(check, driver)
+    while True:
+        schedule.run_pending()
+
+
+if __name__ == '__main__':
+    main()
